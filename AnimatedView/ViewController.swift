83eaf96c61790db3369774a2256d96793104b5c9
Kinjal Pipaliya <kinjalpipaliya88@gmail.com>
2020-04-19 20:53:17 +0530

    Initial Commit

---
+++ AnimatedView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedView
+//
+//  Created by Kinjal Pipaliya on 19/04/20.
+//  Copyright © 2020 Kinjal Pipaliya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
