ad7bac6c4f3cb71f35a181ecd03cd1fb8c284e00
Kinjal Pipaliya <kinjalpipaliya88@gmail.com>
2020-04-19 21:06:49 +0530

    draw()

--- AnimatedView/ViewController.swift
+++ AnimatedView/ViewController.swift
@@ -10,5 +10,26 @@ import UIKit
 
 @IBDesignable class header: UIView
 {
+    @IBInspectable var curveHeight : CGFloat = 50.0
+    var layerheader = CAShapeLayer()
     
+    override func draw(_ rect: CGRect)
+    {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: rect.width, y: 0))
+        path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+        path.addArc(withCenter: CGPoint(x: CGFloat(rect.width) - curveHeight, y: rect.height), radius: curveHeight, startAngle: 0, endAngle: 1.5 * CGFloat.pi, clockwise: false)
+        path.addLine(to: CGPoint(x: curveHeight, y: rect.height - curveHeight))
+        
+        path.addArc(withCenter: CGPoint(x: curveHeight, y: rect.height - (curveHeight * 2.0)), radius: curveHeight, startAngle: 0, endAngle:  CGFloat.pi, clockwise: true)
+               path.close()
+               layerheader.path = path.cgPath
+        layerheader.fillColor = UIColor(red: 65/255, green: 81/255, blue: 107/255, alpha: 1.0).cgColor
+        layerheader.frame = rect
+               self.layer.insertSublayer(layerheader, at: 0)
+               self.layerheader.shadowColor = UIColor.black.cgColor
+               self.layerheader.shadowRadius = 20.0
+               self.layerheader.shadowOpacity = 0.7
+    }
 }
